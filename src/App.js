b4aeed40f31082394b839690496d34c838cdbbca
Anamika Kumari <122178637+Anamika-K20@users.noreply.github.com>
2024-07-16 02:59:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components from react-router-dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import BodySection from './components/BodySection';
 import LivePollPage from './components/livepoll'; // Adjust the import path as per your file structure
 import Login from './components/login'; // Import the Login component
+import Studio from './components/Studio'; // Import the Studio component
 import './components/utils.css'; // Import your CSS file for styling
+import { auth } from '/Users/anamikakumari/myntra-app/src/firebase.js'; // Ensure correct import
+import { useAuthState } from 'react-firebase-hooks/auth';
+import ChatBox from "./components/ChatBox";
+import Welcome from "./components/Welcome";
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <Router>
       <div>
@@ -17,7 +23,9 @@ function App() {
           <Route path="/" element={<BodySection />} />
           <Route path="/live-poll" element={<LivePollPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Studio" element={<Studio />} /> {/* Add route for Studio component */}
         </Routes>
+        {!user ? <Welcome /> : <ChatBox />} {/* Conditionally render Welcome or ChatBox based on user authentication */}
       </div>
     </Router>
   );
